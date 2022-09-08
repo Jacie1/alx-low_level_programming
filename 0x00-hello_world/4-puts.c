5db782c7f1ef6d35ea73f5b4068fed0ab3624331
Jacie1 <irinemwanga5@gmail.com>
2022-09-08 04:52:28 -0700

    hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,3 @@
 #!/bin/bash
-int main(void) {"Programming is like building a multilingual puzzle\n");return(0);}
+#include <stdio.h>
+int main(void) {puts("Programming is like building a multilingual puzzle\n");return(0);}
