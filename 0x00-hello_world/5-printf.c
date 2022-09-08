5ff43fb07d87010f5e47183004171a84478646fc
Jacie1 <irinemwanga5@gmail.com>
2022-09-08 05:12:49 -0700

    print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,9 +8,15 @@
  *
  *   *
  *
- *    * Return: Always 0 (Success)
+ *    * Description: prints "with proper grammar, but the outcome is a piece of
  *
- *     */
+ *     * art,"
+ *
+ *      *
+ *
+ *       * Return: Always 0 (Success)
+ *
+ *        */
 
 int main(void)
 
