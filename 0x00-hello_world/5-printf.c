9f2be09e5553ecc57582520d6cfabdb38253ca78
Jacinta Mutua <110332721+Jacie1@users.noreply.github.com>
2022-09-08 15:29:02 +0300

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry point
  *
- * Description: prints "with proper grammar, but the outcome is a piece of*
- * art,"      
+ * Description: prints "with proper grammar, but the outcome is a piece of
+ * art,"
  *
- * Return: Always 0 (Success) 
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
