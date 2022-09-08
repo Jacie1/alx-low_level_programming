412a557ff2d6262daabe3769a288bbc46bf622ad
Jacie1 <irinemwanga5@gmail.com>
2022-09-08 05:19:59 -0700

    print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,20 +3,13 @@
 
 
 /**
+ * main - Entry point
  *
- *  * main - Entry point
+ * Description: prints "with proper grammar, but the outcome is a piece of*
+ * art,"      
  *
- *   *
- *
- *    * Description: prints "with proper grammar, but the outcome is a piece of
- *
- *     * art,"
- *
- *      *
- *
- *       * Return: Always 0 (Success)
- *
- *        */
+ * Return: Always 0 (Success) 
+ */
 
 int main(void)
 
