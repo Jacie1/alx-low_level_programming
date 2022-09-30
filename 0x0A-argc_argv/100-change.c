4e22aedd73b836381a6e143dffd60c3a48ef6f41
Jacie1 <irinemwanga5@gmail.com>
2022-09-30 13:02:31 -0700

    argc, args

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the minimum number of coins to
+ *        make change for an amount of money.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: If the number of arguments is not exactly one - 1.
+ *         Otherwise - 0.
+ */
+int main(int argc, char *argv[])
+{
+	int cents, coins = 0;
+
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the addition of positive numbers,
+ *        followed by a new line.
+ * @argc: The number of arguments passed to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: If one of the numbers contains symbols that are non-digits - 1.
+ *         Otherwise - 0.
+ */
+int main(int argc, char *argv[])
+{
+	int num, digit, sum = 0;
+
+	for (num = 1; num < argc; num++)
+	{
+
